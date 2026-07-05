421313f79ef14b5200ccfe1c240863eeee9d6fbe
Zath <sathyr2003@gmail.com>
2026-07-05 17:21:26 -0300

    chore: migrar la base de datos de SQLite a PostgreSQL

--- red_social/settings.py
+++ red_social/settings.py
@@ -12,10 +12,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -125,8 +129,13 @@ WSGI_APPLICATION = 'red_social.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': env('DB_NAME'), 
+        'USER': env('DB_USER'), 
+        'PASSWORD': env('DB_PASSWORD'), 
+        'HOST': env('DB_HOST'), 
+        'PORT': env('DB_PORT'), 
+        'CONN_MAX_AGE': 60,
     }
 }
 
