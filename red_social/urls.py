2bdaa18495f854ca6d5592bed5a1ca6147183024
Zath <sathyr2003@gmail.com>
2026-07-05 22:13:02 -0300

    feat: incluir URLs de la app follows en el router principal

--- red_social/urls.py
+++ red_social/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('tweets/', include('tweets.urls')), 
     path('mensajes/', include('mensajes.urls')),
     path('api/conversaciones/', views.api_conversaciones),
-    
+    path('follows/', include('follows.urls')), 
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
