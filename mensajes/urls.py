f5fd8ecac042cdd41cc75da91ae15a46ea0ec23c
Zath <sathyr2003@gmail.com>
2026-07-11 12:51:57 -0300

    feat: agregar ruta api/conversaciones/

--- mensajes/urls.py
+++ mensajes/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('conversacion/<int:user_id>/', views.conversacion, name='conversacion'),
     path('api/enviar/', views.api_enviar_mensaje, name='api_enviar_mensaje'),
     path('api/mensajes/<int:conversacion_id>/', views.api_obtener_mensajes, name='api_obtener_mensajes'),
-    path('api/marcar-leido/<int:mensaje_id>/', views.api_marcar_leido, name='api_marcar_leido')
+    path('api/marcar-leido/<int:mensaje_id>/', views.api_marcar_leido, name='api_marcar_leido'),
+    path('api/conversaciones/', views.api_conversaciones, name='api_conversaciones'),
 ]
