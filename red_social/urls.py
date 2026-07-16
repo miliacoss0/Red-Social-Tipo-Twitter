0fbcf44c5f70b3e5fdb52735e6988460766211c2
denisanna97-rgb <denisanna97@gmail.com>
2026-07-16 00:54:11 -0300

    fix(urls): He corregido los duplicados y el sintaxis de static en urlpatterns

--- red_social/urls.py
+++ red_social/urls.py
@@ -9,22 +9,18 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('usuarios.urls')),
-<<<<<<< HEAD
-
     #URL de tweet
     path('feed/', include('tweets.urls')),                          # Página de feed (tweets)
-
-    
     #URLs de tu app post 
     path('posts/', include('posts.urls')),   
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-    path('posts/', include('posts.urls')),
-    path('feed/', include('tweets.urls')),
     path('tweets/', include('tweets.urls')), 
     path('mensajes/', include('mensajes.urls')),
     path('api/conversaciones/', views.api_conversaciones),
     path('follows/', include('follows.urls')), 
+]
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> ed8504273a7c5c7693cc91ce6ec62d8d81426862
+#Archivo multimedia en modo desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
